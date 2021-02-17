0b8cf1801f8409a34ce10fa27597742cd414b3ad
MatiColo <maticolo14@gmail.com>
2021-02-17 00:07:22 -0300

    Infinite Scroll

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,18 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
 import UsersListItem from "./UsersListItem.js";
 import UserDescription from "./UserDescription.js";
+
 import "./UsersList.css";
 
 const UsersList = () => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
 
-  useEffect(() => {
-    fetch("https://randomuser.me/api/?results=50")
+  const loadFunc = (page) => {
+    if (page == 5) {
+      setHasMore(false);
+    }
+    const url =
+      "https://randomuser.me/api/?page=" + page + "&results=40&seed=mati";
+    fetch(url)
       .then((r) => r.json())
-      .then((json) => setUsers(json.results));
-  }, []);
-
+      .then((json) => {
+        const newUsers = [...users, ...json.results];
+        setUsers(newUsers);
+      });
+  };
   const userComponents = users.map((user) => {
     return (
       <UsersListItem
@@ -24,7 +34,21 @@ const UsersList = () => {
     );
   });
   if (selectedUser == null) {
-    return <div className="users-list">{userComponents}</div>;
+    return (
+      <InfiniteScroll
+        className="users-list"
+        pageStart={0}
+        loadMore={loadFunc}
+        hasMore={hasMore}
+        loader={
+          <div className="loader" key={0}>
+            Loading ...
+          </div>
+        }
+      >
+        {userComponents}
+      </InfiniteScroll>
+    );
   } else {
     return (
       <div className="user-description">
